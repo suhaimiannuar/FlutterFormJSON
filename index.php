5d4cf7f3dfc88496f0aec59c706e37d6d86c4e5b
suhaimiannuar <suhaimi150@gmail.com>
2020-05-20 06:58:54 +0800

    JSON file reference is here

---
+++ index.php
@@ -0,0 +1,417 @@
+<?php
+$json_ugly = '{
+  "title": "G-Form",
+  "backGroundColor": "Blue",
+  "fontColor": "White",
+  "splashScreenLogo": "InternalStorage/Fillit/1041.jpg",
+  "splashAnimation": "fade",
+  "page": [
+    {
+      "pageName": "Testing",
+      "secondLayerNavBar": "true",
+      "refresh": "Auto",
+      "navBarName": [
+        "X",
+        "Y",
+        "Z"
+      ],
+      "X":[
+        {"i": "16,Testing,Nothing now"},
+        {"i": "8,Date1,6/3/2020"},
+        {"i": "8,Date2,6/3/2020"},
+        {"i": "8,Date3,6/3/2020"},
+        {"i": "8,Date4,6/3/2020"},
+        {"i": "8,Date5,6/3/2020"},
+        {"i": "8,Date6,6/3/2020"},
+        {"i": "8,Date7,6/3/2020"},
+        {"i": "8,Date8,6/3/2020"},
+        {"i": "8,Date9,6/3/2020"},
+        {"i": "8,Date0,6/3/2020"},
+        {"i": "8,Date10,6/3/2020"},
+        {"i": "8,Date11,6/3/2020"},
+        {"i": "8,Date12,6/3/2020"},
+        {"i": "8,Date13,6/3/2020"},
+        {"i": "8,Date14,6/3/2020"},
+        {"i": "8,Date15,6/3/2020"},
+        {"i": "8,Date16,6/3/2020"},
+        {"i": "8,Date17,6/3/2020"}
+      ],
+      "Y":[
+        {"i": "6,State,Selangor,Perlis"},
+        {"i": "6,Age,20-24,25-39,40-44,45-49"},
+        {"i": "4,Name,Mamat"},
+        {"i": "5,Longitude,3.1235"},
+        {"i": "0,POST,Submit,http://192.168.0.162:1880/testing"}
+      ],
+      "Z":[
+        {"i": "6,State,Kedah,Johor"},
+        {"i": "6,Gender,Male,Female"},
+        {"i": "10,Alamat rumah,"},
+        {"i": "7,Time wake up,11:23 AM"},
+        {"i": "0,POST,Submit,http://192.168.0.162:1880/testing"}
+      ],
+      "item":[
+      ]
+
+    },
+    {
+      "pageName": "Monitor",
+      "refresh": "Auto",
+      "item": [
+        {
+          "i": "0,GET,Refresh,http://192.168.0.162"
+        },
+        {
+          "i": "2,Controller"
+        },
+        {
+          "i": "3,Activity,Booting"
+        },
+        {
+          "i": "3,Time,20/12/2019 | 13:45:09 | Daylight"
+        },
+        {
+          "i": "3,GPS,Found: 3.0123;101.3421\nConfig: 3.0124;101.3422"
+        },
+        {
+          "i": "3,Last Message 6/21,Not yet/Not yet"
+        },
+        {
+          "i": "14,Logo,https://www.google.com/logos/doodles/2020/stay-home-save-lives-6753651837108752-law.gif"
+        }
+      ]
+    },
+    {
+      "hiddenLevel": "Admin",
+      "pageName": "Config",
+      "headerIcon": "ArrowTop",
+      "headerColor": "White",
+      "headerDir": "Directory B",
+      "refresh": "Manual",
+      "item": [
+        {
+          "i": "2,AIS"
+        },
+        {
+          "i": "4,Sitename,Sg. Klang No. 3"
+        },
+        {
+          "i": "5,MMSI,995331200"
+        },
+        {
+          "i": "5,Radius,2"
+        },
+        {
+          "i": "6,Type,00- Default,01- Reference point,02- RACON,03- Fix structure off-shore,04- Spare for future use,05- Light without sector,06- Light with sector,07- Leading light front,08- Leading light rear,09- Beacon Cardinal N,10- Beacond Cardinal E,11- Beacon Cardinal S,12- Beacon Cardinal W,13- Beacon Port hand,14- Beacon Starboard hand,15- Beacon Preferred Channel port hand,16- Beacon Preferred Channel Starboard hand,17- Beacon Isolated danger,18- Beacon safe water,19- Beacon special mark,20- Cardinal Mark N,21- Cardinal Mark E,22- Cardinal Mark S,23-Cardinal Mark W,24- Port hand Mark,25- Starboard hand Mark,26- Preferred Channel Port hand,27- Preferred Channel Starboard hand,28- Isolated danger,29- Safe water,30- Special mark,31- Light Vessel/LANBY/Rigs"
+        },
+        {
+          "i": "6,EPFS,1- GPS,2- GLONASS,3- Combined GPS/GLONASS,4- Loran-C,5- Chayka,6- Integrated Navigation System,7- Surveyed,8- Galileo"
+        },
+        {
+          "i": "5,Latitude,3.1235"
+        },
+        {
+          "i": "6,Cardinal latitude,N,S"
+        },
+        {
+          "i": "5,Longitude,3.1235"
+        },
+        {
+          "i": "6,Cardinal longitude,E,W"
+        },
+        {
+          "i": "2,Monitoring"
+        },
+        {
+          "i": "6,Use LDR,Yes,No"
+        },
+        {
+          "i": "6,Mon. Sec.,Yes,No"
+        },
+        {
+          "i": "6,Mon. RACON.,Yes,No"
+        },
+        {
+          "i": "0,POST,Submit,http://192.168.0.161:1880/testing"
+        }
+      ]
+    },
+    {
+      "hiddenLevel": "Admin",
+      "pageName": "Work",
+      "headerIcon": "ArrowTop",
+      "headerColor": "White",
+      "headerDir": "Directory B",
+      "refresh": "Manual",
+      "item": [
+        {
+          "i": "2,Installer"
+        },
+        {
+          "i": "10,Name,Hafiz,Suhaimi,Mahathir"
+        },
+        {
+          "i": "2,Activity"
+        },
+        {
+          "i": "6,EPFS,1- GPS,2- GLONASS,3- Combined GPS/GLONASS,4- Loran-C,5- Chayka,6- Integrated Navigation System,7- Surveyed,8- Galileo"
+        },
+        {
+          "i": "7,Time arrived at site,12:39 PM"
+        },
+        {
+          "i": "8,Date,5/3/2020"
+        },
+        {
+          "i": "9,Location GPS,3.1234,N,101.3456,E"
+        },
+        {
+          "i": "10,Problem,"
+        },
+        {
+          "i": "11,SDCard/DCIM/Camera/30123.jpg,BIIT is not functioning"
+        },
+        {
+          "i": "12,Explain problem with more picture"
+        },
+        {
+          "i": "10,Work done,"
+        },
+        {"i": "0,POST,Submit,http://192.168.0.161:1880/testing"}
+      ],
+      "reqField": [
+        "Time arrived at site",
+        "Date",
+        "Problem",
+        "Work done"
+      ]
+    },
+    {
+      "pageName": "Direct",
+      "headerIcon": "Bell",
+      "headerColor": "Red",
+      "headerDir": "Directory A",
+      "refresh": "Auto",
+      "item":[
+        {
+          "i": "15,Amin123,1588595035",
+          "m": [
+            {"p": "Testing testing,1588595035"},
+            {"p": "Skali lagi,1588595121"},
+            {"q": "Ha dapat lagi,1588595130"},
+            {"q": "saje try tulis panjang panjang nak tengok hasil kat app macam mana. OK ke tak?,1588595130"},
+            {"p": "Testing testing,1588595035"},
+            {"p": "Skali lagi,1588595121"},
+            {"q": "Ha dapat lagi,1588595130"},
+            {"q": "saje try tulis panjang panjang nak tengok hasil kat app macam mana. OK ke tak?,1588595130"},
+            {"p": "Testing testing,1588595035"},
+            {"p": "Skali lagi,1588595121"},
+            {"q": "Ha dapat lagi,1588595130"},
+            {"q": "saje try tulis panjang panjang nak tengok hasil kat app macam mana. OK ke tak?,1588595130"},
+            {"p": "Testing testing,1588595035"},
+            {"p": "Skali lagi,1588595121"},
+            {"q": "Ha dapat lagi,1588595130"},
+            {"q": "saje try tulis panjang panjang nak tengok hasil kat app macam mana. OK ke tak?,1588595130"},
+            {"p": "Testing testing,1588595035"},
+            {"p": "Skali lagi,1588595121"},
+            {"q": "Ha dapat lagi,1588595130"},
+            {"q": "saje try tulis panjang panjang nak tengok hasil kat app macam mana. OK ke tak?,1588595130"}
+            ]
+        },
+        {
+          "i": "15,Apis102,1588595035",
+          "m": [
+            {"p": "Kau dah kat mana,1588595035"},
+            {"q": "Otw,1588595100"},
+            {"p": "Cepat sikit,1588595121"},
+            {"q": "Kau balik dulu la,1588595130"}       
+            ]
+        },
+        {
+          "i": "15,Syukor201,1588595035",
+          "m": [
+            {"p": "wei jom balik,1588595035"},
+            {"q": "KK jap,1588595100"}
+            ]
+        }
+      ]
+    },
+    {
+      "pageName": "Map",
+      "headerIcon": "Bell",
+      "headerColor": "Red",
+      "headerDir": "Directory A",
+      "refresh": "Auto",
+      "item": [
+        {
+          "i": "2,RadarMap"
+        },
+        {
+          "i": "13",
+          "Render": [
+            {
+              "Layer0": [
+                {
+                  "gmetry": "rectangle",
+                  "opac": 100,
+                  "color": "white",
+                  "gridDash": 50,
+                  "p0x": 0,
+                  "p0y": 0,
+                  "p1x": 100,
+                  "p1y": 0,
+                  "p2x": 0,
+                  "p2y": 100,
+                  "p3x": 100,
+                  "p3y": 100
+                }
+              ]
+            },
+            {
+              "Layer1": [
+                {
+                  "gmetry": "round",
+                  "opac": 100,
+                  "color": "lightBlue",
+                  "grid": 10,
+                  "p0x": 50,
+                  "p0y": 50,
+                  "radius": 50
+                }
+              ]
+            },
+            {
+              "Layer1": [
+                {
+                  "gmetry": "triangle",
+                  "opac": 100,
+                  "color": "green",
+                  "p0x": 0.123,
+                  "p0y": 0.231,
+                  "p1x": 1.432,
+                  "p1y": 1.234,
+                  "p2x": 4.543,
+                  "p2y": 2.573
+                },
+                {
+                  "gmetry": "triangle",
+                  "opac": 100,
+                  "color": "green",
+                  "p0x": 6.394,
+                  "p0y": 7.532,
+                  "p1x": 7.384,
+                  "p1y": 8.642,
+                  "p2x": 5.321,
+                  "p2y": 7.412
+                },
+                {
+                  "gmetry": "triangle",
+                  "opac": 100,
+                  "color": "blue",
+                  "p0x": 91.451,
+                  "p0y": 90.245,
+                  "p1x": 92.231,
+                  "p1y": 89.563,
+                  "p2x": 90.329,
+                  "p2y": 9.345
+                }
+              ]
+            },
+            {
+              "Layer2": [
+                {
+                  "gmetry": "text",
+                  "opac": 100,
+                  "color": "black",
+                  "style": "bold",
+                  "string": "kapal A",
+                  "p0x": 0.123,
+                  "p0y": 0.231
+                },
+                {
+                  "gmetry": "text",
+                  "opac": 100,
+                  "color": "black",
+                  "style": "bold",
+                  "string": "kapal B",
+                  "p0x": 6.394,
+                  "p0y": 7.532
+                },
+                {
+                  "gmetry": "text",
+                  "opac": 100,
+                  "color": "black",
+                  "style": "bold",
+                  "string": "kapal C",
+                  "p0x": 91.451,
+                  "p0y": 90.245
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "pageName": "Comm",
+      "secondLayerNavBar": "true",
+      "refresh": "Auto",
+      "navBarName": [
+        "Friends",
+        "Breakdown",
+        "Emergency"
+      ],
+      "Friends":[
+        {
+          "i": "15,Amin123,1588595035",
+          "m": [
+            {"p": "Testing testing,1588595035"},
+            {"q": "Okay dapat,1588595100"},
+            {"p": "Skali lagi,1588595121"},
+            {"q": "Ha dapat lagi,1588595130"}            
+            ]
+        },
+        {
+          "i": "15,Apis102,1588595035",
+          "m": [
+            {"p": "Kau dah kat mana,1588595035"},
+            {"q": "Otw,1588595100"},
+            {"p": "Cepat sikit,1588595121"},
+            {"q": "KK,1588595130"},
+            {"q": "Kau balik dulu la,1588595130"}       
+            ]
+        },
+        {
+          "i": "15,Syukor201,1588595035",
+          "m": [
+            {"p": "wei jom balik,1588595035"},
+            {"q": "KK jap,1588595100"},
+            {"p": "hmmm,1588595121"}
+            ]
+        }
+      ],
+      "Breakdown":[
+        {"i": "4,Name,Mamat"},
+        {"i": "5,Longitude,3.1235"},
+        {"i": "0,POST,Submit,http://192.168.0.162:1880/testing"}
+      ],
+      "Emergency":[
+        {"i": "6,State,Kedah,Johor"},
+        {"i": "6,Gender,Male,Female"},
+        {"i": "10,Alamat rumah,"},
+        {"i": "7,Time wake up,11:23 AM"},
+        {"i": "0,POST,Submit,http://192.168.0.162:1880/testing"}
+      ],
+      "item":[
+      ]
+
+    }
+  ]
+}';
+
+$json_pretty = json_encode(json_decode($json_ugly), JSON_PRETTY_PRINT);
+echo $json_pretty;
+
+?>
+
+
